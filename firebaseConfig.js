65a8969a11ee5a451d92cf7d500898a13d589713
Md Arafat Hosen <djarafathd@gmail.com>
2026-02-13 00:25:13 +0600

    Create firebaseConfig.js

---
+++ firebaseConfig.js
@@ -0,0 +1,10 @@
+// firebaseConfig.js
+export const firebaseConfig = {
+apiKey: "AIzaSyDurKi9mAz7dF6IVWY_LkgF19lj-NSt7uQ",
+authDomain: "arafat-chat.firebaseapp.com",
+projectId: "arafat-chat",
+storageBucket: "arafat-chat.firebasestorage.app",
+messagingSenderId: "793298740456",
+appId: "1:793298740456:web:90dac5adeab37d4ce8955d",
+measurementId: "G-DQP3BYWPCH"
+};
